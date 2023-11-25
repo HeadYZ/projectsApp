f3ff915fb2bf323e7d2db3e415da7dee49ca071a
Patryk <krasowski.p@outlook.com>
2023-11-25 20:27:29 +0100

    change NoProjectSelected compoenent name and add that component to App component. In Button component add {...props} to bt

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import NewProject from './components/NewProject.jsx'
 import ProjectsSidebar from './components/ProjectsSidebar.jsx'
+import NoProjectSelected from './components/noProjectSelected.jsx'
 
 function App() {
 	return (
 		<main className='h-screen py-8 flex gap-8  '>
 			<ProjectsSidebar />
-			<NewProject />
+			<NoProjectSelected />
 		</main>
 	)
 }
